99a4ff6277c0fcd567673800fc49bb8e1609e5d9
urzp <urzp002@mail.ru>
2016-06-26 21:46:54 +0300

    plane sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,10 +1,61 @@
+
+
+
 def m_sort(array)
+    
+	if array.size == 2
+	  a = array[0]
+	  b = array[-1]
+	  return a < b ? [a, b] : [b, a]
+	end
+
+	if array.size == 4
+	  a = array.slice(0, array.size / 2)
+	  b = array.slice(array.size / 2, array.size )
+	  a = m_sort(a) 
+	  b = m_sort(b)
+	  c = []
+	  
+	  4.times do
+	    if !a.empty? and !b.empty?
+          if a[0] < b[0]
+	        c << a[0]
+		    a.delete_at(0)
+          else 
+	        c << b[0]
+		    b.delete_at(0)
+	      end
+	    end
+		
+		if a.empty?
+		  c = c + b
+		  break
+		end
+		
+		if b.empty?
+		  c = c + a
+		  break
+		end  
+	  end
+	  
+	  return c
+	end	
+	
+	
+
+    
+    
+
 end
 
 def crate_array (size, width)
   array = []
   size.times do |i|
-    array >> rand(width) + 1
+    array << rand(width) + 1
   end
+  return array
 end
 
+array = [2, 3, 1, 5]
+ 
+print m_sort(array)
