074fa1f6a9f5a88d0ef4c44296ab33c326bb2e0d
urzp <urzp002@mail.ru>
2016-06-26 21:58:07 +0300

    create merage

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,4 +1,30 @@
-
+def merage(a, b)
+  c = []
+  count = a.size + b.size
+  count.times do
+    if !a.empty? and !b.empty?
+      if a[0] < b[0]
+	    c << a[0]
+		a.delete_at(0)
+      else 
+	    c << b[0]
+		b.delete_at(0)
+	  end
+	end
+		
+	if a.empty?
+	  c = c + b
+	  break
+	end
+		
+	if b.empty?
+	  c = c + a
+	  break
+	end  
+  end
+	  
+  return c
+end
 
 
 def m_sort(array)
@@ -14,31 +40,9 @@ def m_sort(array)
 	  b = array.slice(array.size / 2, array.size )
 	  a = m_sort(a) 
 	  b = m_sort(b)
-	  c = []
 	  
-	  4.times do
-	    if !a.empty? and !b.empty?
-          if a[0] < b[0]
-	        c << a[0]
-		    a.delete_at(0)
-          else 
-	        c << b[0]
-		    b.delete_at(0)
-	      end
-	    end
-		
-		if a.empty?
-		  c = c + b
-		  break
-		end
-		
-		if b.empty?
-		  c = c + a
-		  break
-		end  
-	  end
-	  
-	  return c
+	  return merage(a, b)
+
 	end	
 	
 	
@@ -59,3 +63,5 @@ end
 array = [2, 3, 1, 5]
  
 print m_sort(array)
+
+print merage([1,2,3,4], [1,1,2,3,5] )
